4961a7d71aee35b2fde7ebac2c3831c8545fc5e4
sharlinaik <sharlinaik691@gmail.com>
2024-09-24 20:17:03 +0530

    Added basic Notes App with features to add, edit, and delete notes.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import NotesApp from './NotesApp';
+
+function App() {
+  return (
+    <div className="App">
+      <NotesApp />
+    </div>
+  );
+}
+
+export default App;
